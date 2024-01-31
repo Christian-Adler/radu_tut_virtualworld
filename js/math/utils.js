3fe7e7accbec1764da9057c53ba00614092855a6
Christian Adler <chris.adler@gmx.de>
2024-01-31 20:58:05 +0100

    add threshold for selection

--- js/math/utils.js
+++ js/math/utils.js
@@ -2,12 +2,12 @@ function distance(point1, point2) {
     return Math.hypot(point1.x - point2.x, point1.y - point2.y);
 }
 
-function getNearestPoint(loc, points) {
+function getNearestPoint(loc, points, threshold = Number.MAX_SAFE_INTEGER) {
     let minDist = Number.MAX_SAFE_INTEGER;
     let nearest = null;
     for (const point of points) {
         const dist = distance(point, loc);
-        if (dist < minDist) {
+        if (dist < minDist && dist < threshold) {
             minDist = dist;
             nearest = point;
         }
