609e9dcc4521795429c19d67e343497a71ca1b75
Christian Adler <chris.adler@gmx.de>
2024-02-03 05:48:01 +0100

    generate tree positions

--- js/world.js
+++ js/world.js
@@ -16,6 +16,7 @@ class World {
         this.envelopes = [];
         this.roadBorders = [];
         this.buildings = [];
+        this.trees = [];
 
         this.generate();
     }
@@ -30,6 +31,7 @@ class World {
         this.roadBorders = Polygon.union(this.envelopes.map(e => e.poly));
 
         this.buildings = this.#generateBuildings();
+        this.trees = this.#generateTrees();
     }
 
     #generateBuildings() {
@@ -92,6 +94,27 @@ class World {
         return bases;
     }
 
+    #generateTrees(count = 10) {
+        const points = [
+            ...this.roadBorders.map(s => [s.p1, s.p2]).flat(),
+            ...this.buildings.map(b => b.points).flat()
+        ];
+        const left = Math.min(...points.map(p => p.x));
+        const right = Math.max(...points.map(p => p.x));
+        const top = Math.min(...points.map(p => p.y));
+        const bottom = Math.max(...points.map(p => p.y));
+
+        const trees = [];
+        while (trees.length < count) {
+            const p = new Point(
+                lerp(left, right, Math.random()),
+                lerp(bottom, top, Math.random())
+            )
+            trees.push(p);
+        }
+        return trees;
+    }
+
     draw(ctx) {
         for (const envelope of this.envelopes) {
             envelope.draw(ctx, {fill: '#bbb', stroke: '#bbb', lineWidth: 15});
@@ -104,6 +127,9 @@ class World {
         }
         for (const building of this.buildings) {
             building.draw(ctx);
+        }
+        for (const tree of this.trees) {
+            tree.draw(ctx);
         }
     }
 
